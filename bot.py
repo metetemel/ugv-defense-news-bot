d6714cd1bb98d87475b1349f070eedc02fb62fe8
Mete Temel <150029715+metetemel@users.noreply.github.com>
2026-06-08 15:04:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,3 @@
-from fetch import fetch_rss
-from llm import generate_brief
-from telegram import send
-from sources import UGV_RSS, EO_IR_RSS
-
-def run():
-    news_ugv = fetch_rss(UGV_RSS)
-    news_eo = fetch_rss(EO_IR_RSS)
-
-    all_news = news_ugv + news_eo
-
-    report = generate_brief(all_news)
-    send(report)
-
-if __name__ == "__main__":
-    run()
+import sources
+print(dir(sources))
+exit()
