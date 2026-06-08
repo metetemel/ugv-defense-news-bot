27a10cb242eb6e46d2197a11f3164e24529a9f70
Mete Temel <150029715+metetemel@users.noreply.github.com>
2026-06-08 14:31:50 +0300

    Update sources.py

--- sources.py
+++ sources.py
@@ -1,17 +1,12 @@
-UGV_RSS_SOURCES = [
-    {
-        "name": "Defense News - Ground Vehicles",
-        "url": "https://www.defensenews.com/arc/outboundfeeds/rss/category/land/?outputType=xml",
-        "country": "ABD"
-    },
-    {
-        "name": "Army Technology - UGV",
-        "url": "https://www.army-technology.com/tag/unmanned-ground-vehicle/feed/",
-        "country": "Global"
-    },
-    {
-        "name": "Breaking Defense - Land",
-        "url": "https://breakingdefense.com/category/land/feed/",
-        "country": "ABD"
-    }
+# sources.py
+
+UGV_RSS = [
+    "https://www.defensenews.com/arc/outboundfeeds/rss/category/land/?outputType=xml",
+    "https://breakingdefense.com/category/land/feed/",
+    "https://www.army-technology.com/feed/"
+]
+
+EO_IR_RSS = [
+    "https://www.defense-aerospace.com/feed/",
+    "https://www.unmannedairspace.info/feed/"
 ]
